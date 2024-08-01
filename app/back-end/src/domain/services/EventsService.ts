e403fe5dbff7b0088629bf219ac0d0142cff16e1
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-01 19:32:30 -0300

    feat: getParams method
    this method get params from olympic games url from payload fields

--- app/back-end/src/domain/services/EventsService.ts
+++ app/back-end/src/domain/services/EventsService.ts
@@ -1,8 +1,17 @@
 import { BaseService } from './BaseService'
+import { ApiAdapter } from '../../adapters'
 import { IGetEventsPayload, IGetEventsResponse } from '../../interfaces'
 
 export class EventsService extends BaseService {
   getEvents = async (payload: IGetEventsPayload): Promise<IGetEventsResponse> => {
-    return this.repository.events.getEvents(payload)
+    const rawData = await this.repository.events.getEvents(this.getParams(payload))
+
+    const formattedEvents = ApiAdapter.transformGetEventsResponse(rawData)
+
+    return formattedEvents
+  }
+
+  private getParams = (payload: IGetEventsPayload): string => {
+    return ''
   }
 }
