822bfcb61e672e52e5689ba39ffa329e27a2a589
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-01 18:31:24 -0300

    feat: Events payload and response interfaces

--- app/back-end/src/interfaces/Events.ts
+++ app/back-end/src/interfaces/Events.ts
@@ -1,6 +1,58 @@
 export interface IGetEventsPayload {
-
+  country?: string;
+  discipline?: string;
+  venue?: string;
+  date?: Date;
+  competitor?: string;
+  page?: number;
 }
 
 export interface IGetEventsResponse {
+  data: IEvent[];
+  links: ILinks;
+  meta: IMeta;
+}
+
+export interface ICompetitor {
+  countryId: string;
+  countryFlagUrl: string;
+  competitorName: string;
+  position: number;
+  resultPosition: string;
+  resultWinnerLoserTie: string;
+  resultMark: string;
+}
+
+export interface IEvent {
+  id: number;
+  day: string;
+  disciplineName: string;
+  disciplinePictogram: string;
+  name: string | null;
+  venueName: string;
+  eventName: string;
+  detailedEventName: string;
+  startDate: string;
+  endDate: string;
+  status: string;
+  isMedalEvent: number;
+  isLive: number;
+  competitors: ICompetitor[];
+}
+
+export interface ILinks {
+  first: string;
+  last: string;
+  prev: string;
+  next: string;
+}
+
+export interface IMeta {
+  currentPage: number;
+  from: number;
+  lastPage: number;
+  path: string;
+  perPage: number;
+  to: number;
+  total: number;
 }
