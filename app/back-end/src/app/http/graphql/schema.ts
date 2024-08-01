d556553493733e5857f4c31883a6221d1dfd7f8f
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-01 18:42:25 -0300

    feat: getEvents defs

--- app/back-end/src/app/http/graphql/schema.ts
+++ app/back-end/src/app/http/graphql/schema.ts
@@ -4,7 +4,7 @@ import { loadFilesSync } from '@graphql-tools/load-files'
 import { join } from 'path'
 
 import { GraphqlAdapter } from '../../../adapters'
-import { WelcomeController } from '../../../controllers'
+import { EventsController } from '../../../controllers'
 import { DateTime } from 'luxon'
 
 export const baseSchema = loadFilesSync(join(__dirname, 'base.gql'))
@@ -13,16 +13,17 @@ export const typeDefs = loadFilesSync(join(__dirname, 'typedefs.gql'))
 /* istanbul ignore next */
 export const resolvers = {
   Query: {
-    welcome: async (_: any, args: any, context: any) => {
-      const controller = new WelcomeController(context.repository, context.log, context.cache)
-
-      return GraphqlAdapter.perform(controller.getMessage.bind(controller), args, context)
-    },
     health: () => {
       return {
         uptime: process.uptime(),
         timestamp: DateTime.now().toISO(),
       }
+    },
+
+    getEvents: async (_: any, args: any, context: any) => {
+      const controller = new EventsController(context.repository)
+
+      return GraphqlAdapter.perform(controller.getEvents, args, context)
     },
   },
 }
