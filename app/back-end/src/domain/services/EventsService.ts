c666518e417c8ec04190b5f11c1cf165b1aa5257
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-01 18:44:24 -0300

    feat: getEvents method

--- app/back-end/src/domain/services/EventsService.ts
+++ app/back-end/src/domain/services/EventsService.ts
@@ -1,9 +1,8 @@
-import { BaseService } from '.'
-import {
-  IGetEventsPayload,
-  IGetEventsResponse,
-} from '../../interfaces'
+import { BaseService } from './BaseService'
+import { IGetEventsPayload, IGetEventsResponse } from '../../interfaces'
 
 export class EventsService extends BaseService {
-  getEvents = async (payload: IGetEventsPayload): Promise<IGetEventsResponse> => this._repositories.events.getEvents(payload) 
+  getEvents = async (payload: IGetEventsPayload): Promise<IGetEventsResponse> => {
+    return this.repository.events.getEvents(payload)
+  }
 }
