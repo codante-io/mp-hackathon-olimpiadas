c1f02f6f1a9f2d30cd697afdc7d3604880af7242
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-01 18:44:18 -0300

    feat: getEvents method

--- app/back-end/src/controllers/EventsController.ts
+++ app/back-end/src/controllers/EventsController.ts
@@ -1,24 +1,18 @@
+import { BaseController } from './BaseController'
 import { EventsService } from '../domain/services'
 import { EventsPayloadValidator } from '../utils/validations'
-import { 
-  IGetEventsPayload,
-  IGetEventsResponse,
-} from '../interfaces'
-import { BaseController } from './BaseController'
+import { IGetEventsPayload, IGetEventsResponse } from '../interfaces'
 
 export class EventsController extends BaseController {
-  private readonly _eventsService = new EventsService(this._repositories)
+  private readonly eventsService = new EventsService(this.repository)
 
   getEvents = async (payload: IGetEventsPayload): Promise<IGetEventsResponse> => {
-    const {
-      value,
-      error,
-    } = EventsPayloadValidator.validateGetEventsPayload(payload)
+    const { value, error } = EventsPayloadValidator.validateGetEventsPayload(payload)
 
     if (error) {
       throw error
     }
 
-    return this._eventsService.getEvents(value)
+    return this.eventsService.getEvents(value)
   }
 }
