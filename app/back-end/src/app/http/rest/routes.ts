f141578de9047f7a53380cc2f16acfc3db21f546
Leonardo Guimarães Urci <leonardoguimaraesurci@gmail.com>
2024-08-01 18:43:35 -0300

    feat: [GET] events route

--- app/back-end/src/app/http/rest/routes.ts
+++ app/back-end/src/app/http/rest/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { ExpressAdapter } from '../../../adapters'
 import { EventsController } from '../../../controllers'
+import { IContext } from '../../../interfaces'
 
 const router = Router()
 
-export default ({ repository, cache, logger }: any) => {
-  const eventsController = new EventsController(repository, cache, logger)
+export default ({ repository }: IContext) => {
+  const eventsController = new EventsController(repository)
 
   router.get('/events', ExpressAdapter.perform(eventsController.getEvents.bind(eventsController)))
 
