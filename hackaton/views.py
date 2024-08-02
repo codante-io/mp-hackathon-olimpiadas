d154018291ec353532f5753dfa701f89ac8bb723
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2024-08-02 14:32:54 -0300

    Começo agenda e resultados

--- hackaton/views.py
+++ hackaton/views.py
@@ -3,17 +3,53 @@ import json, requests
 from main import app
 from config import *
 
+
 @app.route('/')
-def home(): 
-    response = requests.get(URL)
-    countries = []
-    if response.status_code == 200:
-        data = response.json()
+def filtro():
+    requisicao = requests.get('https://apis.codante.io/olympic-games/disciplines')
+    requisicao = requisicao.json()
+    requisicao = requisicao['data']
+    
+    return render_template('filtro.html', requisicao=requisicao)
 
-        for item in data['data']:
-            
-            countries.append({'nome': item['name'], 'medalha': item['total_medals']})
 
-            countries_sort = sorted(countries, key=lambda x: x['medalha'], reverse=True)  
+@app.route('/calendario-filtrado/', methods=['POST'])
+def process_selection():
+    form_data = request.form.to_dict()
+    esporte = None
+    dia = None
+    try:
+        esporte = form_data.get('selecao_esporte')
+        dia = form_data.get('selecao_dia')
 
-    return render_template('index.html', countries = countries_sort)
+    except:
+        pass
+    
+    requisicao = requests.get(f'https://apis.codante.io/olympic-games/events?date={dia}&discipline={esporte}')
+    requisicao = requisicao.json()
+    requisicao = requisicao['data']
+    return render_template('calendario.html', requisicao=requisicao)
+
+
+
+
+
+
+@app.route('/modalidade/<int:id_pagina>')
+def resultados(id_pagina):
+    proximo = True
+    url = f'https://apis.codante.io/olympic-games/events?page={id_pagina}'
+    resultados = []
+
+    requisicao = requests.get(url)
+    requisicao = requisicao.json()
+    
+    if requisicao['links']['next'] == None:
+        proximo = False
+
+    for i in requisicao['data']:
+        if i['status'] == "Finished":
+            resultados.append(i)
+
+    context = {"id_pagina": id_pagina, "proximo": proximo, "resultados":resultados}
+    return render_template('resultados.html', **context)
